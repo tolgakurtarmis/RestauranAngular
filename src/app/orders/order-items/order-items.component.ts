46063903588842ad5f2ab00bd30a5953cea3809f
tolgakurtarmis <tolgakurtarmis34@gmail.com>
2020-11-30 21:51:10 +0300

    commit

--- src/app/orders/order-items/order-items.component.ts
+++ src/app/orders/order-items/order-items.component.ts
@@ -48,13 +48,24 @@ export class OrderItemsComponent implements OnInit {
   updatePrice(ctrl) {
     if (ctrl.selectedIndex == 0) {
       this.formData.Price = 0;
+      this.formData.ItemName='';
+      this.formData.Total=0;
+
     } else {
-      this.formData.Price = this.itemList[ctrl.selectedIndex - 1].price;
+      this.formData.Price = this.itemList[ctrl.target.selectedIndex - 1].price;
+      this.formData.ItemName = this.itemList[ctrl.target.selectedIndex - 1].name;
+      this.formData.Total=0;
     }
     this.updateTotal();
   }
   updateTotal() {
-    this.formData.Total = parseFloat((this.formData.Quantity * this.formData.Price).toFixed(2));
+    if(this.formData.Quantity == undefined || this.formData.Price == undefined){
+      this.formData.Total = 0;
+    }
+    else{
+      this.formData.Total = parseFloat((this.formData.Quantity * this.formData.Price).toFixed(2));
+      
+    }
   }
   onSubmit(form: NgForm) {
     if(this.validateForm(form.value)){
@@ -64,7 +75,6 @@ export class OrderItemsComponent implements OnInit {
       else{
         this.orderService.orderItemModel[this.data.orderItemIndex]=form.value;
       }
-      this.orderService.orderItemModel.push(form.value);
       console.log("this.orderService.OrderItemModel=>",this.orderService.orderItemModel);      
       this.dialogRef.close();
     }
--- src/app/orders/order/order.component.ts
+++ src/app/orders/order/order.component.ts
@@ -3,7 +3,7 @@ import { OrderItem } from 'src/app/shared/order-item.model';
 import { Order } from 'src/app/shared/order.model';
 import { OrderService } from 'src/app/shared/order.service';
 import { OrderItemsComponent } from '../order-items/order-items.component';
-import { MatDialog,MatDialogConfig } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -13,35 +13,46 @@ import { NgForm } from '@angular/forms';
 })
 export class OrderComponent implements OnInit {
 
-  orderModel :Order = new Order();
-  orderItemModel :Array<OrderItem> = new Array<OrderItem>();
-  
-  constructor(private orderService: OrderService ,
-            private dialog:MatDialog) { }
+  orderModel: Order = new Order();
+  orderItemModel: Array<OrderItem> = new Array<OrderItem>();
+
+  constructor(private orderService: OrderService,
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.orderItemModel = this.orderService.orderItemModel;
-  } 
-
-    resetForm(form?: NgForm){
-      if(form = null)
-          form.resetForm();
-       this.orderModel = {
-         OrderId:null,
-         OrderNo:Math.floor(100000 + Math.random() * 900000).toString(),
-         CustomerId:0,
-         PaymentMethod: "",
-         GrandTotal: 0
-       };
-    }
-
-  AddOrEditOrderItem(orderItemIndex,orderId): void{
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.autoFocus = true;
-    dialogConfig.disableClose=true;
-    dialogConfig.width="500px"; 
-    dialogConfig.data={orderItemIndex,orderId};    
-    this.dialog.open(OrderItemsComponent,dialogConfig);
   }
 
+  resetForm(form?: NgForm) {
+    if (form = null)
+      form.resetForm();
+    this.orderModel = {
+      OrderId: null,
+      OrderNo: Math.floor(100000 + Math.random() * 900000).toString(),
+      CustomerId: 0,
+      PaymentMethod: "",
+      GrandTotal: 0
+    };
+  }
+
+  AddOrEditOrderItem(orderItemIndex, orderId): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.disableClose = true;
+    dialogConfig.width = "500px";
+    dialogConfig.data = { orderItemIndex, orderId };
+    this.dialog.open(OrderItemsComponent, dialogConfig).afterClosed().subscribe(res => {
+      this.updateGrandTotal();
+    });
+  }
+  DeleteOrderItem(orderItemIndex, orderId) {
+    this.orderService.orderItemModel.splice(orderItemIndex, 1);
+    this.updateGrandTotal();
+  }
+  updateGrandTotal() {
+    this.orderModel.GrandTotal = this.orderService.orderItemModel.reduce((prev, curr) => {
+      return prev + curr.Total;
+    }, 0);
+    this.orderModel.GrandTotal = parseFloat(this.orderModel.GrandTotal.toFixed(2));
+  }
 }
